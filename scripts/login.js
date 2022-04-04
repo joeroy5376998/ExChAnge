54a8191cb7767792054cca486e01c0e87d962328
morning <joeroy5376998@gmail.com>
2022-04-04 21:01:45 +0800

    fine tune homepage and login page

--- scripts/login.js
+++ scripts/login.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-    //wirte the control code for login page
+    
+    $("#to_register").click(function(){
+        $("#login").hide();
+        $("#register").show();
+    });
+
+    $("#btn_login").click(function(){
+        // if account and password are correct then rederict to homepage
+        alert("登入成功！")
+        document.location.href="../templates/homepage.html";
+    });
+
+    $("#register").click(function(){
+        alert("註冊成功！將為您跳轉至登入畫面")
+        $("#register").hide();
+        $("#login").show();
+    });
 });
