c5c7948f582bab951326221edf978355cee72b7b
kuanyuchen123 <kuanyu.theme@gmail.com>
2022-04-08 13:09:42 +0800

    Added login required error msg

--- ExChAnge_v1/app/extensions.py
+++ ExChAnge_v1/app/extensions.py
@@ -27,6 +27,7 @@ db = SQLAlchemy(app)
 # Login Manager
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_message = '請先登入才能造訪此頁!'
 login_manager.login_view = 'login'
 
 # Bootstrap
